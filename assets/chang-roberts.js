b06e24965772bf3942fe05e8fa941659bd530612
Sho Iizuka <iizuka.sho.ij4@is.naist.jp>
2016-06-22 18:07:58 +0900

    Modify

--- assets/chang-roberts.js
+++ assets/chang-roberts.js
@@ -1,4 +1,22 @@
 !function () {
+    var width = 940, height = 540;
+    var duration = 350;
+    var ids = [0, 4, 2, 6, 1, 5, 3, 7];
+    var nodeRadius = 30;
+    var messageFontSize = 18;
+    var Point = (function () {
+        function Point(x, y) {
+            this.x = x;
+            this.y = y;
+        }
+        Point.prototype.getX = function () {
+            return this.x;
+        };
+        Point.prototype.getY = function () {
+            return this.y;
+        };
+        return Point;
+    }());
     var Scaler = (function () {
         function Scaler(xScale, yScale) {
             this.xScale = xScale;
@@ -6,61 +24,79 @@
         }
         return Scaler;
     }());
-    var Node = (function () {
-        function Node(id, nodeSize, scaler) {
-            this.id = id;
+    var MessagePositionCalculator = (function () {
+        function MessagePositionCalculator(nodeSize) {
             this.nodeSize = nodeSize;
-            this.scaler = scaler;
-            this.id = id;
-            this.nodeSize = nodeSize;
+            if (nodeSize != 8) {
+                throw new Error("nodeSize != 8");
+            }
         }
-        Node.prototype.computePosition = function (i) {
-            var theta = -2 * Math.PI / this.nodeSize * i;
+        MessagePositionCalculator.prototype.computeRelativePosition = function (i) {
+            var dict = {
+                0: [0, -0.2],
+                1: [0.45, -0.05],
+                2: [0.45, 0],
+                3: [0.45, 0.05],
+                4: [0, 0.25],
+                5: [-0.45, 0.05],
+                6: [-0.45, 0],
+                7: [-0.45, -0.05]
+            };
+            return new Point(dict[i][0], dict[i][1]);
+        };
+        return MessagePositionCalculator;
+    }());
+    var Node = (function () {
+        function Node(id, nodeSize) {
+            this.id = id;
+            this.nodeSize = nodeSize;
+            this.id = id;
+            this.nodeSize = nodeSize;
+            this.messagePositioncalculator = new MessagePositionCalculator(nodeSize);
+        }
+        Node.prototype.getX = function () {
+            return this.point.getX();
+        };
+        Node.prototype.getY = function () {
+            return this.point.getY();
+        };
+        Node.prototype.updateIndexAndPosition = function (index) {
+            this.index = index;
+            this.updatePosition();
+        };
+        Node.prototype.updatePosition = function () {
+            var theta = -2 * Math.PI / this.nodeSize * this.index;
             // x座標もy座標も [-1,1] で指定すれば良い
-            this.x = this.scaler.xScale(-Math.sin(theta));
-            this.y = this.scaler.yScale(-Math.cos(theta));
+            this.point = new Point(-Math.sin(theta), -Math.cos(theta));
+        };
+        Node.prototype.computeMessagePosition = function () {
+            var t = this.messagePositioncalculator.computeRelativePosition(this.index);
+            return new Point(this.getX() + t.getX(), this.getY() + t.getY());
         };
         return Node;
     }());
     var Message = (function () {
         function Message(text) {
             this.text = text;
-            this.text = text;
         }
         return Message;
     }());
-    var width = 940, height = 540;
-    var ids = [0, 4, 2, 6, 1, 5, 3, 7];
-    var nodeRadius = 30;
-    var messageFontSize = 18;
-    var messageTx = -30;
-    var messageTy = -30;
     var xScale = d3.scale
         .linear()
         .domain([-1, 1])
-        .range([width / 2 - height / 2 + nodeRadius, width / 2 + height / 2 - nodeRadius]);
+        .range([width / 2 - height / 2 + 2 * nodeRadius, width / 2 + height / 2 - 2 * nodeRadius]);
     var yScale = d3.scale
         .linear()
         .domain([-1, 1])
-        .range([0 + nodeRadius, height - nodeRadius]);
+        .range([0 + 2 * nodeRadius, height - 2 * nodeRadius]);
     var scaler = new Scaler(xScale, yScale);
     var nodes = [];
-    for (var i = 0; i < ids.length; i++) {
-        var n = ids.length;
-        var node = new Node(ids[i], n, scaler);
-        node.computePosition(i);
-        nodes.push(node);
-    }
     var messages = [];
-    for (var i = 0; i < ids.length; i++) {
-        var message = new Message("<candidate," + ids[i] + ">");
-        message.index = i;
-        messages.push(message);
-    }
     var svg = d3.select(".container")
         .append("svg")
         .attr({ width: width, height: height })
         .classed("center-block", true);
+    var svgBackground = svg.append("g");
     var svgNode = svg.append("g");
     var svgMessage = svg.append("g");
     // https://material.google.com/style/color.html#
@@ -69,44 +105,75 @@
     var colorBlueGrey = "#607D8B";
     var colorWhite = "#FAFAFA";
     var colorBlack = "#212121";
-    // 円を描画
-    svgNode.selectAll("circle")
-        .data(nodes)
-        .enter()
-        .append("circle")
-        .attr({
-        cx: function (d, i) {
-            return d.x;
-        },
-        cy: function (d, i) {
-            return d.y;
-        },
-        r: nodeRadius,
-        fill: colorRed
-    });
-    // 円の中にテキストを描画
-    // "text-anchor": "middle", dy: "0.35em" と設定すると、ちょうどいい感じになる
-    // http://qiita.com/daxanya1/items/734e65a7ca58bbe2a98c
-    svgNode.selectAll("text")
-        .data(nodes)
-        .enter()
-        .append("text")
-        .attr({
-        x: function (d, i) {
-            return d.x;
-        },
-        y: function (d, i) {
-            return d.y;
-        },
-        "text-anchor": "middle",
-        "font-size": nodeRadius,
-        dy: "0.35em",
-        fill: colorWhite
-    })
-        .text(function (d, i) {
-        return d.id;
-    });
-    document.getElementById("button-start").onclick = function () {
+    function initializeBackground() {
+        // 背景の円を描画
+        svgBackground.append("circle")
+            .attr({
+            cx: scaler.xScale(0),
+            cy: scaler.yScale(0),
+            r: scaler.xScale(1) - scaler.xScale(0),
+            stroke: colorBlack,
+            "stroke-width": 2,
+            fill: "none"
+        });
+    }
+    function initializeModel() {
+        for (var i = 0; i < ids.length; i++) {
+            var n = ids.length;
+            var node = new Node(ids[i], n);
+            node.updateIndexAndPosition(i);
+            nodes.push(node);
+        }
+        for (var i = 0; i < ids.length; i++) {
+            var message = new Message("<candidate," + ids[i] + ">");
+            message.index = i;
+            messages.push(message);
+        }
+    }
+    function initializeNodeView() {
+        // ノードを表す円を描画
+        svgNode.selectAll("circle")
+            .data(nodes)
+            .enter()
+            .append("circle");
+        svgNode.selectAll("circle")
+            .data(nodes)
+            .attr({
+            cx: function (d, i) {
+                return scaler.xScale(d.getX());
+            },
+            cy: function (d, i) {
+                return scaler.yScale(d.getY());
+            },
+            r: nodeRadius,
+            fill: colorRed
+        });
+        // 円の中にテキストを描画
+        // "text-anchor": "middle", dy: "0.35em" と設定すると、ちょうどいい感じになる
+        // http://qiita.com/daxanya1/items/734e65a7ca58bbe2a98c
+        svgNode.selectAll("text")
+            .data(nodes)
+            .enter()
+            .append("text");
+        svgNode.selectAll("text")
+            .data(nodes)
+            .attr({
+            x: function (d, i) {
+                return scaler.xScale(d.getX());
+            },
+            y: function (d, i) {
+                return scaler.yScale(d.getY());
+            },
+            "text-anchor": "middle",
+            "font-size": nodeRadius,
+            dy: "0.35em",
+            fill: colorWhite
+        })
+            .text(function (d, i) {
+            return d.id;
+        });
+    }
+    function initializeMessageView() {
         svgMessage.selectAll("text")
             .data(messages)
             .enter()
@@ -115,48 +182,39 @@
             .data(messages)
             .attr({
             x: function (d, i) {
-                return nodes[d.index].x + messageTx;
+                return scaler.xScale(nodes[d.index].computeMessagePosition().getX());
             },
             y: function (d, i) {
-                return nodes[d.index].y + messageTy;
+                return scaler.yScale(nodes[d.index].computeMessagePosition().getY());
             },
             "text-anchor": "middle",
-            "font-size": nodeRadius,
+            "font-size": messageFontSize,
             fill: colorBlack
         })
             .text(function (d, i) { return d.text; });
+    }
+    document.getElementById("button-start").onclick = function () {
+        initializeModel();
+        initializeNodeView();
+        initializeMessageView();
     };
     var counter = 0;
     document.getElementById("button-gt").onclick = function () {
         counter++;
-        for (var i = 0; i < nodes.length; i++) {
-            nodes[i].computePosition((counter + i) % nodes.length);
+        for (var i = 0; i < messages.length; i++) {
+            messages[i].index = (counter + i) % nodes.length;
         }
-        var duration = 350;
-        svgNode.selectAll("circle")
-            .data(nodes)
-            .transition()
-            .duration(duration)
-            .ease("linear")
-            .attr({
-            cx: function (d, i) {
-                return d.x;
-            },
-            cy: function (d, i) {
-                return d.y;
-            }
-        });
-        svgNode.selectAll("text")
-            .data(nodes)
+        svgMessage.selectAll("text")
+            .data(messages)
             .transition()
             .duration(duration)
             .ease("linear")
             .attr({
             x: function (d, i) {
-                return d.x;
+                return scaler.xScale(nodes[d.index].computeMessagePosition().getX());
             },
             y: function (d, i) {
-                return d.y;
+                return scaler.yScale(nodes[d.index].computeMessagePosition().getY());
             }
         });
     };
--- assets/chang-roberts.ts
+++ assets/chang-roberts.ts
@@ -1,73 +1,111 @@
 !function () {
+    const width = 940, height = 540;
+    const duration = 350;
+    const ids = [0, 4, 2, 6, 1, 5, 3, 7];
+    const nodeRadius = 30;
+    const messageFontSize = 18;
+
+    class Point {
+        constructor(private x: number, private y: number) { }
+
+        getX(): number {
+            return this.x;
+        }
+
+        getY(): number {
+            return this.y;
+        }
+    }
+
     class Scaler {
         constructor(
             public xScale: d3.scale.Linear<number, number>,
-            public yScale: d3.scale.Linear<number, number>) {
+            public yScale: d3.scale.Linear<number, number>) { }
+    }
+
+    class MessagePositionCalculator {
+        constructor(public nodeSize: number) {
+            if (nodeSize != 8) {
+                throw new Error("nodeSize != 8");
+            }
+        }
+
+        computeRelativePosition(i: number): Point {
+            const dict: { [index: number]: [number, number] } = {
+                0: [0, -0.2],
+                1: [0.45, -0.05],
+                2: [0.45, 0],
+                3: [0.45, 0.05],
+                4: [0, 0.25],
+                5: [-0.45, 0.05],
+                6: [-0.45, 0],
+                7: [-0.45, -0.05]
+            };
+            return new Point(dict[i][0], dict[i][1]);
         }
     }
 
     class Node {
-        x: number;
-        y: number;
+        index: number;
+        point: Point;
+        messagePositioncalculator: MessagePositionCalculator;
 
-        constructor(public id: number, public nodeSize: number, public scaler: Scaler) {
+        constructor(public id: number, public nodeSize: number) {
             this.id = id;
             this.nodeSize = nodeSize;
+            this.messagePositioncalculator = new MessagePositionCalculator(nodeSize);
         }
 
-        computePosition(i: number) {
-            const theta = -2 * Math.PI / this.nodeSize * i;
+        getX(): number {
+            return this.point.getX();
+        }
+
+        getY(): number {
+            return this.point.getY();
+        }
+
+        updateIndexAndPosition(index: number) {
+            this.index = index;
+            this.updatePosition();
+        }
+
+        updatePosition() {
+            const theta = -2 * Math.PI / this.nodeSize * this.index;
             // x座標もy座標も [-1,1] で指定すれば良い
-            this.x = this.scaler.xScale(-Math.sin(theta));
-            this.y = this.scaler.yScale(-Math.cos(theta));
+            this.point = new Point(-Math.sin(theta), -Math.cos(theta));
+        }
+
+        computeMessagePosition(): Point {
+            const t = this.messagePositioncalculator.computeRelativePosition(this.index);
+            return new Point(this.getX() + t.getX(), this.getY() + t.getY());
         }
     }
 
     class Message {
         index: number;
 
-        constructor(public text: string) {
-            this.text = text;
-        }
+        constructor(public text: string) { }
     }
-
-    const width = 940, height = 540;
-    const ids = [0, 4, 2, 6, 1, 5, 3, 7];
-    const nodeRadius = 30;
-    const messageFontSize = 18;
-    const messageTx = -30;
-    const messageTy = -30;
 
     const xScale = d3.scale
         .linear()
         .domain([-1, 1])
-        .range([width / 2 - height / 2 + nodeRadius, width / 2 + height / 2 - nodeRadius]);
+        .range([width / 2 - height / 2 + 2 * nodeRadius, width / 2 + height / 2 - 2 * nodeRadius]);
     const yScale = d3.scale
         .linear()
         .domain([-1, 1])
-        .range([0 + nodeRadius, height - nodeRadius]);
+        .range([0 + 2 * nodeRadius, height - 2 * nodeRadius]);
     const scaler = new Scaler(xScale, yScale);
 
     const nodes: Node[] = [];
-    for (let i = 0; i < ids.length; i++) {
-        const n = ids.length;
-        const node = new Node(ids[i], n, scaler);
-        node.computePosition(i);
-        nodes.push(node);
-    }
-
     const messages: Message[] = [];
-    for (let i = 0; i < ids.length; i++) {
-        const message = new Message("<candidate," + ids[i] + ">");
-        message.index = i;
-        messages.push(message);
-    }
 
     const svg = d3.select(".container")
         .append("svg")
         .attr({ width: width, height: height })
         .classed("center-block", true);
 
+    const svgBackground = svg.append("g");
     const svgNode = svg.append("g");
     const svgMessage = svg.append("g");
 
@@ -78,46 +116,83 @@
     const colorWhite = "#FAFAFA";
     const colorBlack = "#212121";
 
-    // 円を描画
-    svgNode.selectAll("circle")
-        .data(nodes)
-        .enter()
-        .append("circle")
-        .attr({
-            cx: (d, i) => {
-                return d.x;
-            },
-            cy: (d, i) => {
-                return d.y;
-            },
-            r: nodeRadius,
-            fill: colorRed
-        });
+    function initializeBackground() {
+        // 背景の円を描画
+        svgBackground.append("circle")
+            .attr({
+                cx: scaler.xScale(0),
+                cy: scaler.yScale(0),
+                r: scaler.xScale(1) - scaler.xScale(0),
+                stroke: colorBlack,
+                "stroke-width": 2,
+                fill: "none"
+            });
+    }
 
-    // 円の中にテキストを描画
-    // "text-anchor": "middle", dy: "0.35em" と設定すると、ちょうどいい感じになる
-    // http://qiita.com/daxanya1/items/734e65a7ca58bbe2a98c
-    svgNode.selectAll("text")
-        .data(nodes)
-        .enter()
-        .append("text")
-        .attr({
-            x: (d, i) => {
-                return d.x;
-            },
-            y: (d, i) => {
-                return d.y;
-            },
-            "text-anchor": "middle",
-            "font-size": nodeRadius,
-            dy: "0.35em",
-            fill: colorWhite
-        })
-        .text((d, i) => {
-            return d.id;
-        });
 
-    document.getElementById("button-start").onclick = () => {
+    function initializeModel() {
+        for (let i = 0; i < ids.length; i++) {
+            const n = ids.length;
+            const node = new Node(ids[i], n);
+            node.updateIndexAndPosition(i);
+            nodes.push(node);
+        }
+
+        for (let i = 0; i < ids.length; i++) {
+            const message = new Message("<candidate," + ids[i] + ">");
+            message.index = i;
+            messages.push(message);
+        }
+    }
+
+    function initializeNodeView() {
+        // ノードを表す円を描画
+        svgNode.selectAll("circle")
+            .data(nodes)
+            .enter()
+            .append("circle");
+
+        svgNode.selectAll("circle")
+            .data(nodes)
+            .attr({
+                cx: (d, i) => {
+                    return scaler.xScale(d.getX());
+                },
+                cy: (d, i) => {
+                    return scaler.yScale(d.getY());
+                },
+                r: nodeRadius,
+                fill: colorRed
+            });
+
+        // 円の中にテキストを描画
+        // "text-anchor": "middle", dy: "0.35em" と設定すると、ちょうどいい感じになる
+        // http://qiita.com/daxanya1/items/734e65a7ca58bbe2a98c
+        svgNode.selectAll("text")
+            .data(nodes)
+            .enter()
+            .append("text");
+
+        svgNode.selectAll("text")
+            .data(nodes)
+            .attr({
+                x: (d, i) => {
+                    return scaler.xScale(d.getX());
+                },
+                y: (d, i) => {
+                    return scaler.yScale(d.getY());
+                },
+                "text-anchor": "middle",
+                "font-size": nodeRadius,
+                dy: "0.35em",
+                fill: colorWhite
+            })
+            .text((d, i) => {
+                return d.id;
+            });
+    }
+
+    function initializeMessageView() {
         svgMessage.selectAll("text")
             .data(messages)
             .enter()
@@ -127,49 +202,43 @@
             .data(messages)
             .attr({
                 x: (d, i) => {
-                    return nodes[d.index].x + messageTx;
+                    return scaler.xScale(nodes[d.index].computeMessagePosition().getX());
                 },
                 y: (d, i) => {
-                    return nodes[d.index].y + messageTy;
+                    return scaler.yScale(nodes[d.index].computeMessagePosition().getY());
                 },
                 "text-anchor": "middle",
-                "font-size": nodeRadius,
+                "font-size": messageFontSize,
                 fill: colorBlack
             })
             .text((d, i) => { return d.text; })
     }
 
+    document.getElementById("button-start").onclick = () => {
+        initializeModel();
+        initializeNodeView();
+        initializeMessageView();
+    }
+
     let counter = 0;
     document.getElementById("button-gt").onclick = () => {
         counter++;
-        for (let i = 0; i < nodes.length; i++) {
-            nodes[i].computePosition((counter + i) % nodes.length);
+
+        for (let i = 0; i < messages.length; i++) {
+            messages[i].index = (counter + i) % nodes.length;
         }
-        const duration = 350;
-        svgNode.selectAll("circle")
-            .data(nodes)
-            .transition()
-            .duration(duration)
-            .ease("linear")
-            .attr({
-                cx: (d, i) => {
-                    return d.x;
-                },
-                cy: (d, i) => {
-                    return d.y;
-                },
-            });
-        svgNode.selectAll("text")
-            .data(nodes)
+
+        svgMessage.selectAll("text")
+            .data(messages)
             .transition()
             .duration(duration)
             .ease("linear")
             .attr({
                 x: (d, i) => {
-                    return d.x;
+                    return scaler.xScale(nodes[d.index].computeMessagePosition().getX());
                 },
                 y: (d, i) => {
-                    return d.y;
+                    return scaler.yScale(nodes[d.index].computeMessagePosition().getY());
                 },
             });
     };
